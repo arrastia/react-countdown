1d5d3ad694ceaf1cd08d2853cb96b5e4c930dfb5
Arrastia <arrastia@pm.me>
2020-02-13 19:32:13 +0100

    add useDarkMode custom hook

---
+++ src/interfaces/views/.tools/_hooks/useDarkMode.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+
+export const useDarkMode = () => {
+  const [enabledState, setEnabledState] = useLocalStorage('dark-mode-enabled');
+
+  const prefersDarkMode = usePrefersDarkMode();
+
+  const enabled = typeof enabledState !== 'undefined' ? enabledState : prefersDarkMode;
+
+  useEffect(() => {
+    const className = 'dark-mode';
+    const element = window.document.body;
+    if (enabled) {
+      element.classList.add(className);
+    } else {
+      element.classList.remove(className);
+    }
+  }, [enabled]);
+
+  return [enabled, setEnabledState];
+};
