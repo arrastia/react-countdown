ba6e23475dc24559f197a5d7b6cd007cd418592f
Arrastia <arrastia@pm.me>
2020-02-16 18:02:20 +0100

    update Countdown styles

--- src/_components/Countdown/Countdown.jsx
+++ src/_components/Countdown/Countdown.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
-import "./Countdown.css";
+import React, { useEffect, useState } from 'react';
+
+import './Countdown.css';
 
 export const Countdown = () => {
   useEffect(() => {
@@ -9,17 +10,15 @@ export const Countdown = () => {
   });
 
   const calculateTimeLeft = () => {
-    const difference = new Date("2020-3-30") - new Date();
+    const difference = new Date('2020-5-25') - new Date();
     let timeLeft = {};
 
     if (difference > 0) {
       timeLeft = {
-        days: ("0" + Math.floor(difference / (1000 * 60 * 60 * 24))).slice(-2),
-        hours: ("0" + Math.floor((difference / (1000 * 60 * 60)) % 24)).slice(
-          -2
-        ),
-        minutes: ("0" + Math.floor((difference / 1000 / 60) % 60)).slice(-2),
-        seconds: ("0" + Math.floor((difference / 1000) % 60)).slice(-2)
+        days: ('0' + Math.floor(difference / (1000 * 60 * 60 * 24))).slice(-2),
+        hours: ('0' + Math.floor((difference / (1000 * 60 * 60)) % 24)).slice(-2),
+        minutes: ('0' + Math.floor((difference / 1000 / 60) % 60)).slice(-2),
+        seconds: ('0' + Math.floor((difference / 1000) % 60)).slice(-2)
       };
     }
 
