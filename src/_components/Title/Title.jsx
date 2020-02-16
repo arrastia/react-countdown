4275cab8b59d5fe6c52432c67c47f74f6f8e4e80
Arrastia <arrastia@pm.me>
2020-02-16 18:04:13 +0100

    add Title component

---
+++ src/_components/Title/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Title.css';
+
+export const Title = () => {
+  return (
+    <div className="App-info">
+      <h1 className="App-title">pablo arrastia</h1>
+      <div className="subtitle-wrap" data-hover="See you soon!">
+        <h2 className="subtitle">Working on the new website</h2>
+      </div>
+    </div>
+  );
+};
