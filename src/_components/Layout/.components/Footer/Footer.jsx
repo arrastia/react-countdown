43bbe8f2ba0c4b7cb5ce46b7b75c44cbbd947c57
Arrastia <arrastia@pm.me>
2020-02-16 18:03:06 +0100

    add Footer component

---
+++ src/_components/Layout/.components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Footer.css';
+
+export const Footer = () => {
+  return (
+    <footer className="App-footer">
+      Learn more about{'    '}
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        react.
+      </a>
+    </footer>
+  );
+};
