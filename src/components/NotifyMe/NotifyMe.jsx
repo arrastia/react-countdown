f01dca76bcc966a88416bcbb3e9494bb0ecc29c5
Arrastia <arrastia@pm.me>
2020-03-09 21:38:48 +0100

    update NotifyMe module.scss

--- src/components/NotifyMe/NotifyMe.jsx
+++ src/components/NotifyMe/NotifyMe.jsx
@@ -1,46 +1,34 @@
-import React, { useRef } from "react";
-import "./NotifyMe.css";
+import React, { useRef } from 'react';
+
+import styles from './NotifyMe.module.scss';
 
 export const NotifyMe = () => {
   const inputRef = useRef(null);
+  const checkboxRef = useRef(null);
 
-  const onCloseNotifyMe = () => {
-    document.getElementById("checkbox").checked = false;
-  };
+  const onCloseNotifyMe = () => (document.getElementById('checkbox').checked = false);
 
-  const onFocusInput = () => {
-    console.log("inputRef.current", inputRef.current);
-    inputRef.current.focus();
-  };
+  const onFocusInput = () => inputRef.current.focus();
 
   return (
     <>
-      <input
-        className="c-checkbox"
-        type="checkbox"
-        id="checkbox"
-        onChange={() => onFocusInput()}
-      />
-      <div className="c-formContainer">
-        <form className="c-form" action="">
+      <input id="checkbox" className={styles.checkbox} type="checkbox" onClick={() => onFocusInput()} />
+      <div className={styles.formContainer}>
+        <form className={styles.form} action="">
           <input
-            ref={inputRef}
-            className="c-form__input"
-            placeholder="E-mail"
-            pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$"
-            required
+            className={styles.input}
             // onBlur={() => onCloseNotifyMe()}
+            pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$"
+            placeholder="E-mail"
+            ref={inputRef}
+            required
           />
-          <label className="c-form__buttonLabel" htmlFor="checkbox">
-            <button className="c-form__button" type="button">
+          <label className={styles.buttonLabel} htmlFor="checkbox">
+            <button className={styles.button} type="button">
               Send
             </button>
           </label>
-          <label
-            className="c-form__toggle"
-            htmlFor="checkbox"
-            data-title="Notify me"
-          ></label>
+          <label className={styles.label} htmlFor="checkbox" data-title="Notify me" />
         </form>
       </div>
     </>
