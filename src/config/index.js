39b7ecd772a53a52a98bcd118ebf50b1472ab8e7
Arrastia <arrastia@pm.me>
2020-03-14 19:30:50 +0100

    add language config json

--- src/config/index.js
+++
@@ -1,6 +0,0 @@
-import Icons from "./icons";
-
-const { icons } = Icons;
-const config = (config.icons = icons);
-
-export { config };
---
+++ src/config/lang/index.js
@@ -0,0 +1,5 @@
+import Basque from './lang.eh.json';
+import English from './lang.en.json';
+import Spanish from './lang.es.json';
+
+export const Lang = { ...Basque, ...English, ...Spanish };
