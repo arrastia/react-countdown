b7fb943adaba4bf82a1aca190ba1a4c2774dbd4b
Arrastia <arrastia@pm.me>
2020-03-18 18:05:55 +0100

    delete warnings

--- src/tools/Utils/TextUtils.js
+++ src/tools/Utils/TextUtils.js
@@ -2,7 +2,7 @@ const camelCase = string =>
   string
     .split('_')
     .map(function(word, index) {
-      if (index == 0) {
+      if (index === 0) {
         return word.toLowerCase();
       }
       return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
